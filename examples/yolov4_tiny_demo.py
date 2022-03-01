db07c601bb9ae285cbd7a985eccc66f81b623bb9
wuhanstudio <wuhanstudio@qq.com>
2022-03-01 11:56:36 +0000

    [model][detection] Add YOLOv4

---
+++ examples/yolov4_tiny_demo.py
@@ -0,0 +1,36 @@
+import cv2
+
+from what.models.detection.datasets.coco import COCO_CLASS_NAMES
+from what.models.detection.utils.box_utils import draw_bounding_boxes
+from what.models.detection.yolo.yolov4_tiny import YOLOV4_TINY
+
+# Capture from camera
+cap = cv2.VideoCapture(0)
+cap.set(3, 1920)
+cap.set(4, 1080)
+
+# Darknet
+model = YOLOV4_TINY(COCO_CLASS_NAMES, "models/yolov4-tiny.h5")
+
+while True:
+    _, orig_image = cap.read()
+    if orig_image is None:
+        continue
+
+    # Image preprocessing
+    image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+
+    # Run inference
+    images, boxes, labels, probs = model.predict(image, 10, 0.4)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # Draw bounding boxes onto the image
+    output = draw_bounding_boxes(image, boxes, labels, model.class_names, probs);
+
+    cv2.imshow('YOLOv3 MobileNet', image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
