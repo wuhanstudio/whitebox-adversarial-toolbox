3c6eaecc130fa987bc470225e259d0b4b58017ce
wuhanstudio <wuhanstudio@qq.com>
2022-03-24 11:06:47 +0000

    [fix] logging level

--- what/utils/logger.py
+++ what/utils/logger.py
@@ -1,14 +1,16 @@
 import logging
 
-def get_logger(name):
+def get_logger(name, level=logging.INFO):
+    logging.basicConfig()
     logger = logging.getLogger(name)
     logger.handlers = [] # This is the key thing for the question!
 
     # Start defining and assigning your handlers here
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s %(name)s: %(message)s"))
-    handler.setLevel(logging.INFO)
+    handler.setLevel(level)
     logger.handlers = [handler]
     logger.propagate = False
+    logger.setLevel(level)
 
     return logger
