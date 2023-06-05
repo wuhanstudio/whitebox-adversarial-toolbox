4e54b98b33bbd590d838aa4b93d29b58094cd46b
wuhanstudio <wuhanstudio@qq.com>
2023-06-05 21:53:28 +0100

    [cli][model] Add SSD and FRCNN

--- what/_main.py
+++ what/_main.py
@@ -8,12 +8,15 @@ from what.utils.file import get_file
 MODEL_PATH =  os.path.join(Path.home(), '.what', 'models')
 
 what_model_list = [
-    ('YOLOv3      ( Darknet )', 'Object Detection', 'YOLOv3 pretrained on MS COCO dataset.', 'yolov3.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov3.h5', 'e557c671de8c46ff7d83a9a9b9750bcb2958a7275638c931106ada5d6057a26d'),
-    ('YOLOv3      (Mobilenet)', 'Object Detection', 'YOLOv3 pretrained on MS COCO dataset.', 'yolov3_mobilenet_lite_416_coco.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov3_mobilenet_lite_416_coco.h5', '93eb7be204fca8dd8bafa5146b9795978c9ca6a0ba1d2e2c002f4ed0a1322436'),
-    ('YOLOv3 Tiny ( Darknet )', 'Object Detection', 'YOLOv3 Tiny pretrained on MS COCO dataset.', 'yolov3-tiny.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov3-tiny.h5', '344387880e8ff5e45a313c85f2eeeb2fa4f6d3511ed1e5611aaed438db1f3876'),
-    ('YOLOv3 Tiny (MobileNet)', 'Object Detection', 'YOLOv3 Tiny pretrained on MS COCO dataset.', 'tiny_yolo3_mobilenet_lite_416_coco.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/tiny_yolo3_mobilenet_lite_416_coco.h5', 'e124316a47915936baa39a157aca58cac86813b2c9bf49646e26c24e80252000'),
-    ('YOLOv4      ( Darknet )', 'Object Detection', 'YOLOv4 pretrained on MS COCO dataset.', 'yolov4.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov4.h5', '54802f99cdbddcb0f31180a55d30be1a80d0b73edaa13e9152829318387512e4'),
-    ('YOLOv4 Tiny ( Darknet )', 'Object Detection', 'YOLOv4 Tiny pretrained on MS COCO dataset.', 'yolov4-tiny.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov4-tiny.h5', '3d6f1a3d0540dd3d807de79cb0ad3374b5cdf7495abef5f89505f1c0fcbf8911')
+    ('YOLOv3      (    Darknet    )', 'Object Detection', 'YOLOv3 pretrained on MS COCO dataset.', 'yolov3.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov3.h5', 'e557c671de8c46ff7d83a9a9b9750bcb2958a7275638c931106ada5d6057a26d'),
+    ('YOLOv3      (   Mobilenet   )', 'Object Detection', 'YOLOv3 pretrained on MS COCO dataset.', 'yolov3_mobilenet_lite_416_coco.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov3_mobilenet_lite_416_coco.h5', '93eb7be204fca8dd8bafa5146b9795978c9ca6a0ba1d2e2c002f4ed0a1322436'),
+    ('YOLOv3 Tiny (    Darknet    )', 'Object Detection', 'YOLOv3 Tiny pretrained on MS COCO dataset.', 'yolov3-tiny.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov3-tiny.h5', '344387880e8ff5e45a313c85f2eeeb2fa4f6d3511ed1e5611aaed438db1f3876'),
+    ('YOLOv3 Tiny (   MobileNet   )', 'Object Detection', 'YOLOv3 Tiny pretrained on MS COCO dataset.', 'tiny_yolo3_mobilenet_lite_416_coco.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/tiny_yolo3_mobilenet_lite_416_coco.h5', 'e124316a47915936baa39a157aca58cac86813b2c9bf49646e26c24e80252000'),
+    ('YOLOv4      (    Darknet    )', 'Object Detection', 'YOLOv4 pretrained on MS COCO dataset.', 'yolov4.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov4.h5', '54802f99cdbddcb0f31180a55d30be1a80d0b73edaa13e9152829318387512e4'),
+    ('YOLOv4 Tiny (    Darknet    )', 'Object Detection', 'YOLOv4 Tiny pretrained on MS COCO dataset.', 'yolov4-tiny.h5', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/yolov4-tiny.h5', '3d6f1a3d0540dd3d807de79cb0ad3374b5cdf7495abef5f89505f1c0fcbf8911'),
+    ('SSD         ( MobileNet  v1 )', 'Object Detection', 'SSD pretrained on MS COCO dataset.', 'mobilenet-v1-ssd-mp-0_675.pth', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/mobilenet-v1-ssd-mp-0_675.pth', '58694cafa60456eeab4e81ae50ff49a01c46ab387bfea5200f047143ecd973a9'),
+    ('SSD         ( MobileNet  v2 )', 'Object Detection', 'SSD pretrained on MS COCO dataset.', 'mobilenet-v2-ssd-lite-mp-0_686.pth', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/mobilenet-v2-ssd-lite-mp-0_686.pth', 'b0d1ac2cdbf3c241ba837f51eeebc565ea37b95b7258e2604506a2f991e398a4'),
+    ('FasterRCNN  (     VGG16     )', 'Object Detection', 'FasterRCNN pretrained on MS COCO dataset.', 'fasterrcnn_12211511_0.701052458187_torchvision_pretrain.pth', 'https://wuhanstudio.nyc3.cdn.digitaloceanspaces.com/what/fasterrcnn_12211511_0.701052458187_torchvision_pretrain.pth', '3fd279284b536da3eac754404779e32e2e9fdd82d8511bbc7f6c50e14f0c69d2')
 ]
 
 what_attack_list = [
