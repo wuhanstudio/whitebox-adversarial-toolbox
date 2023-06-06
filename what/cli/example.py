0fdb450795cfbb5dc9acb5af513bd121e3d1f969
wuhanstudio <wuhanstudio@qq.com>
2023-06-06 20:45:12 +0100

    [cli][example] Add FRCNN example

--- what/cli/example.py
+++ what/cli/example.py
@@ -1,5 +1,6 @@
 from what.examples.yolov3_demo import yolov3_inference_demo
 from what.examples.yolov4_demo import yolov4_inference_demo
+from what.examples.faster_rcnn_demo import frcnn_inference_demo
 
 WHAT_EXAMPLE_NAME_INDEX = 0
 WHAT_EXAMPLE_TYPE_INDEX = 1
@@ -7,6 +8,7 @@ WHAT_EXAMPLE_DESC_INDEX = 2
 WHAT_EXAMPLE_FUNC_INDEX = 3
 
 what_example_list = [
-    ('Yolov3 Demo', ' Model Inference ', 'Yolov3 Object Detection.', yolov3_inference_demo),
-    ('Yolov4 Demo', ' Model Inference ', 'Yolov4 Object Detection.', yolov4_inference_demo),
+    ('  Yolov3 Demo  ', ' Model Inference ', 'Yolov3 Object Detection.', yolov3_inference_demo),
+    ('  Yolov4 Demo  ', ' Model Inference ', 'Yolov4 Object Detection.', yolov4_inference_demo),
+    ('FasterRCNN Demo', ' Model Inference ', ' FRCNN Object Detection.', frcnn_inference_demo),
 ]
---
+++ what/examples/faster_rcnn_demo.py
@@ -0,0 +1,83 @@
+import cv2
+import torch
+import numpy as np
+
+from what.models.detection.frcnn.faster_rcnn import FasterRCNN
+# from what.models.detection.frcnn.datasets.util import read_image
+
+from what.cli.model import *
+from what.utils.file import get_file
+
+from what.models.detection.utils.box_utils import draw_bounding_boxes
+from what.models.detection.datasets.voc import VOC_CLASS_NAMES
+
+def frcnn_inference_demo():
+
+    video = input(f"Please input the OpenCV capture device (e.g. 0, 1, 2): ")
+
+    while not video.isdigit():
+        video = input(f"Please input the OpenCV capture device (e.g. 0, 1, 2): ")
+
+    # Capture from camera
+    cap = cv2.VideoCapture(int(video))
+    #cap.set(3, 1920)
+    #cap.set(4, 1080)
+
+    # Download the model first if not exists
+    # Check what_model_list for all available models
+    index = 8
+    if not os.path.isfile(os.path.join(WHAT_MODEL_PATH, what_model_list[index][WHAT_MODEL_FILE_INDEX])):
+        get_file(what_model_list[index][WHAT_MODEL_FILE_INDEX],
+                 WHAT_MODEL_PATH,
+                 what_model_list[index][WHAT_MODEL_URL_INDEX],
+                 what_model_list[index][WHAT_MODEL_HASH_INDEX])
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    while True:
+        _, orig_image = cap.read()
+        if orig_image is None:
+            continue
+
+        # Image preprocessing
+        input_img = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+        height, width, _ = input_img.shape
+
+        # NHWC -> NCHW
+        input_img = np.array(input_img).transpose((2, 0, 1))
+        input_img = torch.from_numpy(input_img)[None]
+
+        # img = read_image('notebooks/demo.jpg', format='NCHW')
+        # input_img = torch.from_numpy(img)[None]
+        # RGB --> BGR
+        # img = img.transpose((1, 2, 0)).astype(np.uint8)
+
+        model = FasterRCNN(device=device)
+        model.load(os.path.join(WHAT_MODEL_PATH, what_model_list[8][WHAT_MODEL_FILE_INDEX]), map_location=device)
+
+        inputs, boxes, labels, scores = model.predict(input_img)
+
+        # (x1, y1, x2, y2) --> (c1, c2, w, h) (0.0, 1.0)
+        boxes = np.array(boxes)[0]
+        box_w  = boxes[:, 2] - boxes[:, 0]
+        box_h = boxes[:, 3] - boxes[:, 1]
+        boxes[:, 0] += box_w / 2
+        boxes[:, 0] /= width
+        boxes[:, 1] += box_h / 2
+        boxes[:, 1] /= height
+        boxes[:, 2] = box_w / width
+        boxes[:, 3] = box_h / height
+
+        output = draw_bounding_boxes(orig_image,
+                boxes,
+                labels[0],
+                VOC_CLASS_NAMES[1:],
+                scores[0])
+
+        cv2.imshow('Faster RCNN', output)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
