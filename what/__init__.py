177eb4b77269c54a4aeb5d7eab755af9d93dea01
wuhanstudio <wuhanstudio@qq.com>
2023-06-08 20:38:12 +0100

    [pypi] Add what.examples module

--- what/__init__.py
+++ what/__init__.py
@@ -4,8 +4,11 @@ WHite-box Adversarial Toolbox (WHAT) is a python library for Deep Learning Secur
 This module implements several object detection models.
 ## what.attacks
 This module implements several white-box attacks against Deep Learning models.
+## what.examples
+This module implements several examples, including model inference and adversarial attacks.
 ## what.utils
 This module implements several utility functions.
+
 '''
 
 # Project Imports
@@ -14,4 +17,4 @@ from what import attacks
 from what import utils
 
 # Semantic Version
-__version__ = "0.0.1"
+__version__ = "0.1.0"
---
+++ what/examples/__init__.py
@@ -0,0 +1,11 @@
+r'''
+This module implements several examples, including model inference and adversarial attacks.
+'''
+
+from what.examples.yolov3_demo import yolov3_inference_demo
+from what.examples.yolov4_demo import yolov4_inference_demo
+from what.examples.faster_rcnn_demo import frcnn_inference_demo
+from what.examples.mobilenet_ssd_demo import mobilenet_ssd_inference_demo
+
+from what.examples.yolov3_tog_attack_demo import yolov3_tog_attack_demo
+from what.examples.yolov3_pcb_attack_demo import yolov3_pcb_attack_demo
