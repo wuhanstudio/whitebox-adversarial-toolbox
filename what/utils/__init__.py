c930ad90cfca42d7e4901a9fb90e5656da509a73
Han Wu <wuhanstudio@qq.com>
2023-06-09 15:59:06 +0100

    Update __init__.py

--- what/utils/__init__.py
+++ what/utils/__init__.py
@@ -1,5 +1,12 @@
 r'''
 Utility functions for WHAT.
+
+<br />
+
+## what.utils.logger.get_logger
+## what.utils.resize.bilinear_resize
+## what.utils.proj_lp.proj_lp
+
 '''
 
 from what.utils.logger import get_logger
