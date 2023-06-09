0f023bc6424992c35901121d635378dd17d70a31
wuhanstudio <wuhanstudio@qq.com>
2023-06-09 15:21:31 +0100

    [doc] Add documentation for examples

--- what/examples/__init__.py
+++ what/examples/__init__.py
@@ -1,5 +1,42 @@
 r'''
 This module implements several examples, including model inference and adversarial attacks.
+
+Use `what example run` to run examples.
+
+```
+           Demo                Type             Description
+--------------------------------------------------------------------------------
+1 :     Yolov3 Demo      Model Inference        Yolov3 Object Detection.
+2 :     Yolov4 Demo      Model Inference        Yolov4 Object Detection.
+3 :   FasterRCNN Demo    Model Inference        FRCNN Object Detection.
+4 : MobileNet SSD Demo   Model Inference        MobileNet SSD Object Detection.
+5 :  TOG Attack Demo    Adversarial Attack      Real-time TOG Attack against Yolov3 Tiny.
+6 :  PCB Attack Demo    Adversarial Attack      Real-time PCB Attack against Yolov3 Tiny.
+
+Please input the example index: 
+```
+
+<br />
+
+# Model Inference:
+
+Object Detection Demos.
+
+## what.examples.yolov3_demo
+## what.examples.yolov4_demo
+
+## what.examples.faster_rcnn_demo
+## what.examples.mobilenet_ssd_demo
+
+<br />
+
+# Adversarial Attacks:
+
+Real-time Adversarial Attacks against Object Detection.
+
+## what.examples.yolov3_tog_attack_demo
+## what.examples.yolov3_pcb_attack_demo
+
 '''
 
 from what.examples.yolov3_demo import yolov3_inference_demo
