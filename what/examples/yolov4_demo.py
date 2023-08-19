07757a88b0a718b2e242e5cd81dbca553df32bbe
wuhanstudio <wuhanstudio@qq.com>
2023-08-19 15:39:54 +0100

    [fix] grammar error

--- what/examples/yolov4_demo.py
+++ what/examples/yolov4_demo.py
@@ -41,10 +41,10 @@ def yolov4_inference_demo():
                 WHAT_YOLOV4_MODEL_HASH)
 
     if index == 0:
-        model = YOLOV4(COCO_CLASS_NAMES, os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE]))
+        model = YOLOV4(COCO_CLASS_NAMES, os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE))
 
     if index == 1:
-        model = YOLOV4_TINY(COCO_CLASS_NAMES, os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE]))
+        model = YOLOV4_TINY(COCO_CLASS_NAMES, os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE))
 
     video = input(f"Please input the OpenCV capture device (e.g. 0, 1, 2): ")
 
