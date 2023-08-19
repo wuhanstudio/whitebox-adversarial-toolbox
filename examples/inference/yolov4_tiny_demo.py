f50d1ddea788ffffb356918b8126c11673fb9027
Han Wu <wuhanstudio@qq.com>
2023-08-19 13:54:09 +0100

    Update yolov4_tiny_demo.py

--- examples/inference/yolov4_tiny_demo.py
+++ examples/inference/yolov4_tiny_demo.py
@@ -34,7 +34,7 @@ if not os.path.isfile(os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE)):
              WHAT_YOLOV4_MODEL_HASH)
 
 # Darknet
-model = YOLOV4_TINY(COCO_CLASS_NAMES, os.path.join(WHAT_MODEL_PATH, what_yolov4_model_list[index][WHAT_MODEL_FILE_INDEX]))
+model = YOLOV4_TINY(COCO_CLASS_NAMES, os.path.join(WHAT_MODEL_PATH, WHAT_YOLOV4_MODEL_FILE))
 
 # You can also use your own model
 # model = YOLOV4_TINY(COCO_CLASS_NAMES, "models/yolov4-tiny.h5")
