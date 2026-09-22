65a06159042952049f613ee3f9ac35d6ed29ec1a
wuhanstudio <wuhanstudios@gmail.com>
2026-09-22 12:20:24 +0100

    [what] v0.2.4

--- setup.py
+++ setup.py
@@ -52,9 +52,18 @@ setuptools.setup(
     url="https://github.com/wuhanstudio/whitebox-adversarial-toolbox",
     license="MIT",
     install_requires=install_requires,
-    extra_require = {
-        "dev": [    "progressbar",
-        ]
+    extras_require = {
+        "dev": [
+            "pytest",
+            "pdoc",
+            "build",
+        ],
+        "test": [
+            "pytest",
+        ],
+        "docs": [
+            "pdoc",
+        ],
     },
     entry_points={
         'console_scripts': [
