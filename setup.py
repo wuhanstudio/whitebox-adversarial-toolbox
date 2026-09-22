3f693e2290774a6432563d3e348780c489dbaf7d
wuhanstudio <wuhanstudios@gmail.com>
2026-09-22 12:09:08 +0100

    [ci] Limit numpy version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_version(rel_path):
     raise RuntimeError("Unable to find version string.")
 
 install_requires = [
-    "numpy>=1.18.0",
+    "numpy>=1.18.0,<1.20",
     "tqdm",
     "six",
     "setuptools",
